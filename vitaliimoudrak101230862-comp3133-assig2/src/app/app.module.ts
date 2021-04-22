c88324502ce1033f9ba8ef114fe2c092462c564f
Vitalii Moudrak <101230862@georgebrown.ca>
2021-04-22 00:46:18 -0400

    Update app.module.ts

--- vitaliimoudrak101230862-comp3133-assig2/src/app/app.module.ts
+++ vitaliimoudrak101230862-comp3133-assig2/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
-
 
 @NgModule({
   declarations: [
