ffed8d20bbb6d7e91514825801c1d361538280ac
Vitalii Moudrak <101230862@georgebrown.ca>
2021-04-22 15:17:53 -0400

    updated backend
    bkend

--- vitaliimoudrak101230862-comp3133-assig2/src/app/app.module.ts
+++ vitaliimoudrak101230862-comp3133-assig2/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -12,7 +11,6 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    GraphQLModule,
     HttpClientModule
   ],
   providers: [],
