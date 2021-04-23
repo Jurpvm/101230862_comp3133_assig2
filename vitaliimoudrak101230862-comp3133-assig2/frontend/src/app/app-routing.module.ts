04f0fd0d29eb02d1eedc74ff5c385eb785b88d66
Vitalii Moudrak <101230862@georgebrown.ca>
2021-04-23 10:12:10 -0400

    Updated app routing
    Updated app routing

--- vitaliimoudrak101230862-comp3133-assig2/frontend/src/app/app-routing.module.ts
+++ vitaliimoudrak101230862-comp3133-assig2/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BookComponent } from './book/book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'book',
+    component: BookComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
